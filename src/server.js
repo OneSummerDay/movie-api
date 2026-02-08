144c958adde82b44ea8dab0978018e1c06666d68
OneSummerDay <artem.kraievyi@gmail.com>
2026-02-08 20:34:38 +0200

    feat: initialize database connection in server.js with error handling

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { config } from 'dotenv';
+import { connectDB, disconnectDB } from './config/db.js';
 
 // Import routes
 import movieRoutes from './routes/movieRoutes.js';
 
 config();
+connectDB();
 
 const app = express();
 
@@ -15,3 +17,28 @@ const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled Rejection:', err);
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(1);
+  });
+});
+
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', (err) => {
+  console.error('SIGTERM received');
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(0);
+  });
+});
+
