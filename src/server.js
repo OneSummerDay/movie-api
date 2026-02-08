2643b91ec4fd4c39b113a42d8dd9afecf588ce94
OneSummerDay <artem.kraievyi@gmail.com>
2026-02-08 18:51:45 +0200

    chore: add script to run dev server

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
