54900f458feaae38050812f0bf8d17cb1e052f8e
OneSummerDay <artem.kraievyi@gmail.com>
2026-02-10 20:31:48 +0200

    feat: add user signup with password hashing

---
+++ src/controllers/authController.js
@@ -0,0 +1,40 @@
+import { status } from 'express/lib/response.js';
+import { prisma } from '../config/db.js';
+import bcrypt from 'bcryptjs';
+
+const register = async (req, res) => {
+    const {name, email, password} = req.body;
+    
+    const userExist = await prisma.user.findUnique({
+        where: { email: email }
+    });
+
+    if (userExist) {
+        return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await prisma.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    });
+
+    res.status(201).json({ 
+        status: 'success',
+        data: {
+            user: {
+                id: user.id,
+                name: user.name,
+                email: user.email
+            }
+        }
+     });
+}
+
+
+export { register };
