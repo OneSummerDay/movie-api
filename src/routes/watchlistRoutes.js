2419069365095ba8e33f856eb98779cd85757314
OneSummerDay <artem.kraievyi@gmail.com>
2026-02-15 16:33:30 +0200

    feat: register addToWatchList controller in watchlist router

---
+++ src/routes/watchlistRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { addToWatchList } from "../controllers/watchlistController.js";
+
+const router = express.Router();
+
+router.post('/', addToWatchList);
+router.post('/login', );
+router.post('/logout', );
+
+
+export default router;
