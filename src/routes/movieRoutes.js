77c547af79292de96d76a71258503ca28fa21651
OneSummerDay <artem.kraievyi@gmail.com>
2026-02-15 16:47:52 +0200

    refactor: minor code cleanup

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -6,6 +6,22 @@ router.get('/movies', (req, res) => {
     res.json({ message: 'List of movies' });
 })
 
+router.get('/movies', (req, res) => {
+    res.json({ message: 'List of movies' });
+})
+
+router.post('/movies', (req, res) => {
+    res.json({ message: 'List of movies' });
+})
+
+router.put('/movies', (req, res) => {
+    res.json({ message: 'List of movies' });
+})
+
+router.delete('/movies', (req, res) => {
+    res.json({ message: 'List of movies' });
+})
+
 
 export default router;
 
