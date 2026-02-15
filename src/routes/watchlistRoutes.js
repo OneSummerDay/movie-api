e87eedf918f26a0060c458bb56c26104a0c72e83
OneSummerDay <artem.kraievyi@gmail.com>
2026-02-15 17:35:34 +0200

    feat: add DELETE route to watchlist routes

--- src/routes/watchlistRoutes.js
+++ src/routes/watchlistRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { addToWatchList } from "../controllers/watchlistController.js";
+import authMiddleware from "../middlewares/authMiddlewar.js";
 
 const router = express.Router();
 
+router.use(authMiddleware);
+
 router.post('/', addToWatchList);
-router.post('/login', );
-router.post('/logout', );
+router.delete('/:movieID', removeFromWatchList);
 
 
 export default router;
